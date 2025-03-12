e99e2f9fcf70bdf6664d645fea33edd6114324b6
matt n <mathiasnitz@gmx.de>
2025-03-12 13:47:28 +0000

    FirstCommit

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post("/api/shorturl", );
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
